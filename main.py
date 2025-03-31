0153831a8e1b39d42750c599de2d7bc97ff168a6
DemidAntipin <138116940+DemidAntipin@users.noreply.github.com>
2025-03-31 11:16:37 +0800

    Update maket dashboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+import dash_bootstrap_components as dbc
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 numeric_columns = df.select_dtypes(include=['number']).columns.tolist()
@@ -8,28 +9,34 @@ not_year_options = [col for col in numeric_columns if col != 'year']
 # external CSS stylesheets
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    html.H4(children='Выберите страну', style={'textAlign':'right'}),
-    dcc.Dropdown(options=[{'label': country, 'value': country} for country in df.country.unique()],
-                 value=['Canada'], id='dropdown-selection', multi=True),
-    html.H4(children='Выберите метрику', style={'textAlign':'right'}),
-    dcc.Dropdown(options=[{'label': col, 'value': col} for col in numeric_columns], value='pop', id='dropdown-y-selection'),
-    html.H4(children='Линейный график', style={'textAlign':'center'}),
-    dcc.Graph(id='graph-content'),
-    html.H4(children='Выберите метрику X', style={'textAlign':'right'}),
-    dcc.Dropdown(options=[{'label': col, 'value': col} for col in not_year_options], value='pop', id='scatter-x'),
-    html.H4(children='Выберите метрику Y', style={'textAlign':'right'}),
-    dcc.Dropdown(options=[{'label': col, 'value': col} for col in not_year_options], value='pop', id='scatter-y'),
-    html.H4(children='Выберите метрику, отвечающую за размер пузырьков', style={'textAlign':'right'}),
-    dcc.Dropdown(options=[{'label': col, 'value': col} for col in not_year_options], value='pop', id='size-selector'),
-    html.H4(children='Выберите год', style={'textAlign':'right'}),
-    dcc.Dropdown(options=[{'label': year, 'value': year} for year in df.year.unique()], value=df.year[0], id='year-selector'),
-    dcc.Graph(id='bubble-graph'),
-    dcc.Graph(id='top15-graph'),
-    dcc.Graph(id='continent-pop-graph')
+    dbc.Row([
+        dbc.Col([html.H4(children='Выберите страну', style={'textAlign':'center'}),
+                dcc.Dropdown(options=[{'label': country, 'value': country} for country in df.country.unique()], value=['Canada'], id='dropdown-selection', multi=True)]),
+        dbc.Col([html.H4(children='Выберите метрику', style={'textAlign':'center'}),
+                dcc.Dropdown(options=[{'label': col, 'value': col} for col in numeric_columns], value='pop', id='dropdown-y-selection')])
+    ]),
+    dbc.Col([html.H4(children='Линейный график', style={'textAlign':'center'}),
+            dcc.Graph(id='graph-content')]),
+    dbc.Row([
+        dbc.Col([html.H4(children='Выберите метрику X', style={'textAlign':'center'}),
+                dcc.Dropdown(options=[{'label': col, 'value': col} for col in not_year_options], value='pop', id='scatter-x')]),
+        dbc.Col([html.H4(children='Выберите метрику Y', style={'textAlign':'center'}),
+                dcc.Dropdown(options=[{'label': col, 'value': col} for col in not_year_options], value='pop', id='scatter-y')]),
+        dbc.Col([html.H4(children='Выберите год', style={'textAlign':'center'}),
+                dcc.Dropdown(options=[{'label': year, 'value': year} for year in df.year.unique()], value=df.year[0], id='year-selector')])
+    ]),
+    dbc.Col([
+        dbc.Col([dbc.Col([html.H4(children='Выберите метрику, отвечающую за размер пузырьков', style={'textAlign':'center'}),
+                          dcc.Dropdown(options=[{'label': col, 'value': col} for col in not_year_options], value='pop', id='size-selector'),
+                          dcc.Graph(id='bubble-graph')])
+        ]),
+        dbc.Row([dbc.Col(dcc.Graph(id='top15-graph'), width=8),
+                 dbc.Col(dcc.Graph(id='continent-pop-graph'), width=4)])
+    ]),
 ])
 
 @callback(
